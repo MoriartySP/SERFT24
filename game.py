602eeddc238128baafed41b41dec87c5fd965181
Sean Moriarty <Moriarty.SeanP@gmail.com>
2024-03-04 17:30:00 -0600

    First commit

---
+++ game.py
@@ -0,0 +1,9 @@
+"""Driver file to demonstrate imports."""
+
+import spotipy
+from player import greet_player
+
+print("Here I am in game.py")
+
+# print("In game.py:" + __name__)
+greet_player()
---
+++ player.py
@@ -0,0 +1,21 @@
+"""Module containing one function"""
+
+
+def greet_player() :
+    player = input('Who are you? ')
+    print(f"Hello, {player}")
+
+# def random():
+
+# print(random())
+
+# print("In player.py:" + __name__)
+# greet_player() # Comment out this line for demo
+
+
+# Uncomment this section for demo
+
+if __name__ == '__main__':
+
+  # Things to do when run  
+  greet_player()
